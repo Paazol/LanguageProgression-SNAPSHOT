d1dea40acec40772aff3bafb1b0bc7035ba7cb4c
Heavenset <eboyhehe70@gmail.com>
2024-07-14 13:50:20 +0300

    This code is starting to make more sence

--- src/main/java/com/Tracker/LanguageProgression/Controller/RegisterController.java
+++ src/main/java/com/Tracker/LanguageProgression/Controller/RegisterController.java
@@ -2,10 +2,8 @@ package com.Tracker.LanguageProgression.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import lombok.Data;
-
-@Data
 @Controller
 public class RegisterController {
 	
