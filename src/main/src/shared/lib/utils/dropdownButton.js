b2711651f39ee60da9dd2f427bf3bcf4d2efbecc
Heavenset <eboyhehe70@gmail.com>
2024-08-18 21:55:09 +0300

    21

--- src/main/src/shared/lib/utils/dropdownButton.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const levelButton = document.querySelector(".suggestedLevelOfEnglishBUTTON");
-    const levelMenu = document.querySelector(".levelCheckboxesDIV");
-
-    levelButton.addEventListener("click", function(event) {
-        event.preventDefault();
-        levelMenu.classList.toggle("levelCheckboxes-transition");
-    });
-});
