2ad63b7595a6d66537cc79871eebac9620e11c50
Heavenset <eboyhehe70@gmail.com>
2024-08-25 21:49:36 +0300

    hfg

--- run.bat
+++ run.bat
@@ -1,2 +1,4 @@
-cd cd d:\EclipseProjects\LanguageProgression-SNAPSHOT
+pg_ctl start -D "C:\Program Files\PostgreSQL\16\data"
+cd d:\EclipseProjects\LanguageProgression-SNAPSHOT
 mvn spring-boot:run
+PAUSE
