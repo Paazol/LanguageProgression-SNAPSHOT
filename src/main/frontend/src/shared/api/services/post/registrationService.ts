19c905a6c42cdcc5304df6bf893645e6537b8588
Heavenset <eboyhehe70@gmail.com>
2024-10-22 21:08:40 +0300

    AUTO PUSH

--- src/main/frontend/src/shared/api/services/post/registrationService.ts
+++ src/main/frontend/src/shared/api/services/post/registrationService.ts
@@ -1,6 +1,5 @@
 import React from "react";
-import Cookies from 'js-cookie';
-
+import {getCookies} from '../../../lib/utils/getCookies.js'
 const registrationService = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
@@ -12,41 +11,28 @@ const registrationService = async (e: React.FormEvent<HTMLFormElement>) => {
     const password = formData.get("password") as string;
     const levelOfEnglish = formData.get("levelOfEnglish") as string;
 
-    async function getCsrfToken() {
-        const response = await fetch('http://localhost:8080/security/csrf-token', {
-            method: "GET",
-            credentials: 'include'
-        });
-        if (!response.ok) {
-            throw new Error('Failed to fetch CSRF token');
-        }
-        const csrfToken = await response.json();
-
-        return Cookies.set("X-CSRF-TOKEN", csrfToken.token, { expires: 1 });
-    }
-
-    let csrfToken = Cookies.get("X-CSRF-TOKEN") as string;
-
-    if(!csrfToken) {
-        csrfToken = await getCsrfToken() as string;
-    }
-
+    let getCsrfToken = await fetch("http://localhost:8080/security/csrf-token", {
+        method: "GET",
+        credentials: "include"
+    });
+   
+    
+    let csrfToken = await getCookies();
     console.log(csrfToken);
     const response = await fetch('http://localhost:8080/register', {
         method: 'POST',
         headers: {
-            "X-CSRF-TOKEN": csrfToken,
             'Content-Type': 'application/json',
+            'X-CSRF-TOKEN': csrfToken.token
         },
-        body: JSON.stringify({ username, email, password, levelOfEnglish })
+        body: JSON.stringify({username, email, password, levelOfEnglish})
     });
 
     if (!response.ok) {
         const errorResponse = await response.json();
         throw new Error(errorResponse.message || 'Registration failed');
     }
-
     return await response.json();
-};
+    }
 
 export default registrationService;
