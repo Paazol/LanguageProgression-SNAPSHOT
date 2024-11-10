368aa0858633b94660cf699e31b342a50c839ca7
Heavenset <eboyhehe70@gmail.com>
2024-11-10 23:59:49 +0300

    AUTO PUSH

--- src/main/java/com/Tracker/LanguageProgression/Configuration/SecurityConfiguration.java
+++ src/main/java/com/Tracker/LanguageProgression/Configuration/SecurityConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -63,7 +64,11 @@ public class SecurityConfiguration {
         return configuration.getAuthenticationManager();
     }
 
-    // How on earth do i fix dat thing??????
+    @Bean
+    public CsrfTokenRepository csrfTokenRepository() {
+    	return new CookieCsrfTokenRepository();
+    }
+    
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
