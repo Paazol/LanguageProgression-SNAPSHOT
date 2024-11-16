6a2c75352b8101fefc42ff4847835e71bfa89bbe
Heavenset <eboyhehe70@gmail.com>
2024-11-16 23:08:42 +0300

    AUTO PUSH

--- src/main/frontend/src/shared/lib/utils/getCsrfToken.ts
+++ src/main/frontend/src/shared/lib/utils/getCsrfToken.ts
@@ -1,19 +1,8 @@
-import { useState, useEffect } from "react";
-
 async function getCsrfToken(name: string, url: string) {
-    const [csrfToken, setCsrfToken] = useState<string | null>(null)
-    useEffect(() => {
-        // Fetch CSRF token from the backend
-        fetch('http://localhost:8080/security/csrf-token')
-            .then(response => response.text())
-            .then(token => {
-                // Set the fetched token in the state
-                setCsrfToken(token);
-            })
-            .catch(error => {
-                console.error('Error fetching CSRF token:', error);
-            });
-    }, []);
+        fetch('http://localhost:8080/security/csrf-token', 
+            
+        );
+
 }
 
 export default getCsrfToken;
